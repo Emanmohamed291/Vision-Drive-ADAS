859b9b35c0ebb0a44773bbcc1e2f155849334683
AlaaIbrahim <aalaa.h.ibrahim@gmail.com>
2024-06-20 11:55:46 +0200

    add tim2 interrupt

--- FreeRTOS-Version/Core/Src/tim.c
+++ FreeRTOS-Version/Core/Src/tim.c
@@ -159,6 +159,9 @@ void HAL_TIM_IC_MspInit(TIM_HandleTypeDef* tim_icHandle)
 
     __HAL_AFIO_REMAP_TIM2_PARTIAL_1();
 
+    /* TIM2 interrupt Init */
+    HAL_NVIC_SetPriority(TIM2_IRQn, 5, 0);
+    HAL_NVIC_EnableIRQ(TIM2_IRQn);
   /* USER CODE BEGIN TIM2_MspInit 1 */
 
   /* USER CODE END TIM2_MspInit 1 */
@@ -225,6 +228,8 @@ void HAL_TIM_IC_MspDeInit(TIM_HandleTypeDef* tim_icHandle)
 
     HAL_GPIO_DeInit(Echo2_GPIO_Port, Echo2_Pin);
 
+    /* TIM2 interrupt Deinit */
+    HAL_NVIC_DisableIRQ(TIM2_IRQn);
   /* USER CODE BEGIN TIM2_MspDeInit 1 */
 
   /* USER CODE END TIM2_MspDeInit 1 */
